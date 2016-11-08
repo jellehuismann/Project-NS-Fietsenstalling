7abbe2da3cafef16697c61bcbf74021b72d24539
jellehuismann <jellehuisman@outlook.com>
2016-11-08 15:33:38 +0100

    Functie om te kijken in de database toegevoegd.

--- Jelle Code/Database Test/Test_Database.py
+++ Jelle Code/Database Test/Test_Database.py
@@ -14,6 +14,26 @@ print("Database Created")
 
 theCursor = db_conn.cursor()
 #Database aanmaken
+def print_database():
+    try:
+        result = theCursor.execute("SELECT ID, Naam, Achternaam, Adress, Telefoon, FietsNr, PIN, otpKEY FROM Fietsenstalling")
+
+        # You receive a list of lists that hold the result
+        for row in result:
+            print("Naam :", row[0])
+            print("Achternaam :", row[1])
+            print("Adress :", row[2])
+            print("Telefoon :", row[3])
+            print("FietsNr :", row[4])
+            print("PIN :", row[5])
+            print("otpKEY :", row[6])
+
+    except sqlite3.OperationalError:
+        print("The Table Doesn't Exist")
+
+    except:
+        print("Couldn't Retrieve Data From Database")
+
 def create_database():
     try:
         db_conn.execute('CREATE TABLE IF NOT EXISTS Fietsenstalling (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
@@ -32,4 +52,5 @@ def add_to_database():
     db_conn.commit()
 
 add_to_database()
+print(print_database())
 
