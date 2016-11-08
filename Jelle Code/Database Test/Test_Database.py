a19d54ca805e7e0867b77ac585436384a4f50ede
jellehuismann <jellehuisman@outlook.com>
2016-11-08 15:18:40 +0100

    Database.py verdeeld in functies

--- Jelle Code/Database Test/Test_Database.py
+++ Jelle Code/Database Test/Test_Database.py
@@ -1,7 +1,7 @@
 import sqlite3
 import sys
 
-Naam = 'aaa'
+Naam = 'bbb'
 Achternaam = 'aaa'
 Adress = 'aaa'
 FietsNr = 2343
@@ -13,18 +13,23 @@ db_conn = sqlite3.connect('database.db')
 print("Database Created")
 
 theCursor = db_conn.cursor()
+#Database aanmaken
+def create_database():
+    try:
+        db_conn.execute('CREATE TABLE IF NOT EXISTS Fietsenstalling (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
+                        'Naam TEXT ,Achternaam TEXT ,Adress TEXT ,Telefoon INTEGER ,FietsNr INTEGER ,PIN INTEGER ,otpKEY TEXT)')
+        db_conn.commit()
+        print("Table Created")
 
-try:
-    db_conn.execute('CREATE TABLE IF NOT EXISTS Fietsenstalling (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
-                    'Naam TEXT ,Achternaam TEXT ,Adress TEXT ,Telefoon INTEGER ,FietsNr INTEGER ,PIN INTEGER ,otpKEY TEXT)')
+    except sqlite3.OperationalError:
+        print("Table couldn't be Created")
+
+#Import Naar Database
+def add_to_database():
+    db_conn.execute("INSERT INTO Fietsenstalling (Naam, Achternaam, Adress, FietsNr, PIN, otpKEY) VALUES "
+                    "(?, ?, ?, ?, ?, ?);",(Naam, Achternaam, Adress, FietsNr, PIN, otpKEY))
+
     db_conn.commit()
-    print("Table Created")
 
-except sqlite3.OperationalError:
-    print("Table couldn't be Created")
-
-db_conn.execute("INSERT INTO Fietsenstalling (Naam, Achternaam, Adress, FietsNr, PIN, otpKEY) VALUES "
-                "(?, ?, ?, ?, ?, ?);",(Naam, Achternaam, Adress, FietsNr, PIN, otpKEY))
-
-db_conn.commit()
+add_to_database()
 
