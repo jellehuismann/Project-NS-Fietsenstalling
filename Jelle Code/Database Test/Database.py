cb06ac443865e8ad3f560ff273ac1bd73fd5657c
jellehuismann <jellehuisman@outlook.com>
2016-11-08 15:15:00 +0100

    Database aangepast voor het toevoegen van een variable naar database

--- Jelle Code/Database Test/Database.py
+++ Jelle Code/Database Test/Database.py
@@ -1,6 +1,4 @@
 import sqlite3
-import sys
-
 Naam = 'aaa'
 Achternaam = 'aaa'
 Adress = 'aaa'
@@ -9,22 +7,25 @@ PIN = 3343
 otpKEY = 'aaa'
 
 db_conn = sqlite3.connect('database.db')
+dbcursor = db_conn.cursor()
 
-print("Database Created")
-
-theCursor = db_conn.cursor()
-
-try:
+def create_table():
     db_conn.execute('CREATE TABLE IF NOT EXISTS Fietsenstalling (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
                     'Naam TEXT ,Achternaam TEXT ,Adress TEXT ,Telefoon INTEGER ,FietsNr INTEGER ,PIN INTEGER ,otpKEY TEXT)')
-    db_conn.commit()
-    print("Table Created")
+    dbcursor.commit()
+print(create_table())
 
-except sqlite3.OperationalError:
-    print("Table couldn't be Created")
+def printdb():
+    try:
+        Result = db_conn.execute("SELECT ID, Name, FietsNr FROM Fietsenstalling")
+        for row in Result:
+            print("ID :", row[0])
+            print("Name :", row[1])
+            print("FietsNr :", row[2])
+    except sqlite3.OperationalError:
+        return "The table doesn't Exist"
 
 db_conn.execute("INSERT INTO Fietsenstalling (Naam, Achternaam, Adress, FietsNr, PIN, otpKEY) VALUES "
                 "(?, ?, ?, ?, ?, ?);",(Naam, Achternaam, Adress, FietsNr, PIN, otpKEY))
 
-db_conn.commit()
-
+db_conn.close()
